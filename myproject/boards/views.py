51779e623783ad7aeebeaf3ccc0e5d8748a9d3ff
xaiosi <zhangqiyouxiang2019@outlook.com>
2020-11-20 16:17:16 +0800

    first-time

---
+++ myproject/boards/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Board
+
+# Create your views here.
+def home(request):
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
